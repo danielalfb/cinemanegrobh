e7cd59cd4fd2c2b000baeec76f0ac90ed72cbdcd
danielalfb <danielalfbarbosa@gmail.com>
2021-03-27 08:32:35 -0300

    início do raciocínio para o cine-escrituras

--- semanadecinemanegro/js/cineEscrituras.js
+++ semanadecinemanegro/js/cineEscrituras.js
@@ -2,36 +2,27 @@ var container = document.querySelector('.container');
 var src = './img/filmes/';
 var boxContent = [
   {
-    tituloPt: 'A MORTE BRANCA DO FEITICEIRO NEGRO',
-    tituloEng: 'THE EHITE DEATH OF THE BLACK WIZARD',
-    localDuracao: 'SANTA CATARINA, 2020, 10"',
+    tituloPt: 'ATÉ O FIM',
+    tituloEng: ' ',
+    localDuracao: 'BAHIA, 2020, 93’',
     fichaTecnica:
-      '<li>direção director Rodrigo Ribeiro</li> <li>roteiro script Rodrigo Ribeiro / Timóteo</li><li>produção production  Julia Faraco / Rodrigo Ribeiro / Luiz Gustavo Laurindo</li><li>fotografia cinematography Carlos Adelino / Rodrigo Ribeiro</li><li>montagem editing Rodrigo Ribeiro / Carlos Eduardo Ceccon / Julia Faraco</li><li>som sound Juçara Marçal / Cadu Tenório</li><li>edição de som sound edition Rodrigo Ribeiro / Leandro Cordeiro</li><li>empresa produtora production company Gata Maior Filmes</li><li>contato contact (e-mail) contato@gatamaior.com.br</li><li>Classificação Indicativa 16 anos</li>',
+      '<li>direção director Ary Rosa, Glenda Nicácio</li><li>roteiro script Ary Rosa</li> <li>fotografia cinematography Augusto Bortolini, Poliana Costa, Thacle de Souza</li><li>arte e figurino art and costume design Glenda Nicácio</li><li>som sound Napoleão Cunha</li><li>elenco principal main cast Maíra Azevedo, Arlete Dias, Wal Diaz, Jenny Muler</li><li>empresa produtora production company Rosza Filmes Produções</li><li>contato contact roszafilmesproducoes@gmail.com</li><li>Classificação Indicativa 14 anos</li>',
     sinopsePt:
-      'Mem�rias do passado escravista brasileiro transbordam em paisagens et�reas e ru�dos angustiantes. Atrav�s de um po�tico ensaio visual, uma reflex�o sobre silenciamento e invisibiliza��o do povo preto em di�spora, numa jornada �ntima e sensorial.Mem�rias do passado escravista brasileiro transbordam em paisagens et�reas e ru�dos angustiantes. Atrav�s de um po�tico ensaio visual, uma reflex�o sobre silenciamento e invisibiliza��o do povo preto em di�spora, numa jornada �ntima e sensorial.',
+      'Geralda está trabalhando em seu quiosque à beira de uma praia no Recôncavo da  Bahia, ela recebe um telefonema do hospital dizendo que seu pai pode morrer a qualquer momento.  Ela avisa suas irmãs Rose, Bel e Vilmar. O encontro promovido pela espera da morte se torna um  momento de desabafo e reconhecimentos das quatro irmãs que não se reúnem desde a morte da  mãe, há 15 anos.',
     sinopseEng:
-      'Memories of the Brazilian slavery past overflow into ethereal landscapes and harrowing noises. Through a visual poetic essay, an intimate and sensory journey reflects on the silencing and invisibility of black people in diaspora.',
-    img: 'MB_01',
-    link: 'https://www.youtube.com/watch?v=lyrEsJLQa-c',
-  },
-  {
-    tituloPt: 'A MORTE BRANCA DO FEITICEIRO NEGRO',
-    tituloEng: 'THE EHITE DEATH OF THE BLACK WIZARD',
-    localDuracao: 'SANTA CATARINA, 2020, 10"',
-    fichaTecnica:
-      '<li>direção director Rodrigo Ribeiro</li> <li>roteiro script Rodrigo Ribeiro / Timóteo</li><li>produção production  Julia Faraco / Rodrigo Ribeiro / Luiz Gustavo Laurindo</li><li>fotografia cinematography Carlos Adelino / Rodrigo Ribeiro</li><li>montagem editing Rodrigo Ribeiro / Carlos Eduardo Ceccon / Julia Faraco</li><li>som sound Juçara Marçal / Cadu Tenório</li><li>edição de som sound edition Rodrigo Ribeiro / Leandro Cordeiro</li><li>empresa produtora production company Gata Maior Filmes</li><li>contato contact (e-mail) contato@gatamaior.com.br</li><li>Classificação Indicativa 16 anos</li>',
-    sinopsePt:
-      'Mem�rias do passado escravista brasileiro transbordam em paisagens et�reas e ru�dos angustiantes. Atrav�s de um po�tico ensaio visual, uma reflex�o sobre silenciamento e invisibiliza��o do povo preto em di�spora, numa jornada �ntima e sensorial.',
-    sinopseEng:
-      'Memories of the Brazilian slavery past overflow into ethereal landscapes and harrowing noises. Through a visual poetic essay, an intimate and sensory journey reflects on the silencing and invisibility of black people in diaspora.',
-    img: 'MB_01',
-    link: 'https://www.youtube.com/watch?v=lyrEsJLQa-c',
+      'Geralda is working at her stand by the beach in Reconcavo da Bahia; she receives a call from the hospital saying that her father might die at any moment. She gives the news to her sisters: Rose, Bel, and Vilmar. The reunion caused by the wait for death becomes a moment for getting things off their chests and acknowledgements for the four sisters who are meeting for the first time since the death of their mother, 15 years ago. ',
+    img: 'CEP_ATEOFIM.png',
+    imgDir: 'maybe',
+    link: '/',
   },
 ];
 
 function createData() {
   for (i = 0; i < boxContent.length; i++) {
     var postBox = document.createElement('div');
+    var sesTitle = document.createElement('h1');
+    sesTitle.className = 'sessionTitle';
+    sesTitle.innerHTML = 'SESSÃO ESPECIAL';
     postBox.className = 'postBox';
     postBox.innerHTML =
       '<div class="postImg filmeImg" style="background-image: url(&#39;' +
@@ -53,6 +44,7 @@ function createData() {
       '</ul></div></div><a href="' +
       boxContent[i].link +
       '" class="btn" target="_blank">Assista o filme <i class="fas fa-external-link-alt"></i></a></div></div></div></div>';
+    container.appendChild(sesTitle);
     container.appendChild(postBox);
   }
 }
