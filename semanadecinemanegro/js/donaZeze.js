efe0595ffade07ef5f6d56df4f6732056936a7a6
danielalfb <danielalfbarbosa@gmail.com>
2021-03-29 13:28:13 -0300

    refatoração do script

--- semanadecinemanegro/js/donaZeze.js
+++ semanadecinemanegro/js/donaZeze.js
@@ -46,27 +46,34 @@ function createData() {
   for (i = 0; i < boxContent.length; i++) {
     var postBox = document.createElement('div');
     postBox.className = 'postBox';
-    postBox.innerHTML =
-      '<div class="postImg filmeImg" style="background-image: url(&#39;' +
-      src +
-      boxContent[i].img +
-      '&#39;)"> </div>' +
-      '<div class="postInfo"><h1>' +
-      boxContent[i].tituloPt +
-      '</h1><h2>' +
-      boxContent[i].tituloEng +
-      '</h2><h3>' +
-      boxContent[i].localDuracao +
-      '</h3><p>' +
-      boxContent[i].sinopsePt +
-      '</p><p>' +
-      boxContent[i].sinopseEng +
-      '</p><div class="btn-filmes"><div class="info"><button type="button" class="collapsible"> MAIS INFORMAÇÕES <i class="fas fa-plus"></i></button><div class="content"> <ul>' +
-      boxContent[i].fichaTecnica +
-      '</ul></div></div><a href="' +
-      boxContent[i].link +
-      '" class="btn" target="_blank">Assista o filme <i class="fas fa-external-link-alt"></i></a></div></div></div></div>';
+    postBox.innerHTML = `
+    <div class="postImg filmeImg" style="background-image: url('${src}${boxContent[i].img}')"></div>
+    <div class="postInfo"><h1>${boxContent[i].tituloPt}</h1><h2>${boxContent[i].tituloEng}</h2><h3>${boxContent[i].localDuracao}</h3>
+    <p>${boxContent[i].sinopsePt}</p><p>${boxContent[i].sinopseEng}</p>
+    <div class="btn-filmes"><div class="info"><button type="button" class="collapsible"> MAIS INFORMAÇÕES <i class="fas fa-plus"></i></button>
+    <div class="content"><ul>${boxContent[i].fichaTecnica}</ul></div></div>
+    <a href="${boxContent[i].link}" class="btn" target="_blank">Assista o filme <i class="fas fa-external-link-alt"></i></a></div></div></div></div>`;
     container.appendChild(postBox);
   }
 }
 createData();
+
+// '<div class="postImg filmeImg" style="background-image: url(&#39;' +
+//       src +
+//       boxContent[i].img +
+//       '&#39;)"> </div>' +
+//       '<div class="postInfo"><h1>' +
+//       boxContent[i].tituloPt +
+//       '</h1><h2>' +
+//       boxContent[i].tituloEng +
+//       '</h2><h3>' +
+//       boxContent[i].localDuracao +
+//       '</h3><p>' +
+//       boxContent[i].sinopsePt +
+//       '</p><p>' +
+//       boxContent[i].sinopseEng +
+//       '</p><div class="btn-filmes"><div class="info"><button type="button" class="collapsible"> MAIS INFORMAÇÕES <i class="fas fa-plus"></i></button><div class="content"> <ul>' +
+//       boxContent[i].fichaTecnica +
+//       '</ul></div></div><a href="' +
+//       boxContent[i].link +
+//       '" class="btn" target="_blank">Assista o filme <i class="fas fa-external-link-alt"></i></a></div></div></div></div>';
