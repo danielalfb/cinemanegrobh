68ae307eb9309d2b14884b626effd2c2cda1fa42
Lidiane Pereira <lidiane.pereira@dtidigital.com.br>
2021-03-31 12:06:48 -0300

    video nos creditos

--- semanadecinemanegro/creditos.php
+++ semanadecinemanegro/creditos.php
@@ -38,6 +38,36 @@ getHeader();
         </div>
       </div>
     </section>
-<?php
-  getFooter();
+    <section>
+    <div class="div-video">
+      <div class="video-footer">
+        <button id = "button"><em class = "fa fa-play" aria-hidden = "true"></em></button>
+        <div id = "lightbox">
+          <em id = "close-btn" class="fa fa-times"></em>
+          <div id = "video-wrapper">
+            <iframe class="youtube-video" title="Video Inicial" id = "video" width="960" height="540" src = "https://www.youtube.com/embed/lJfqK9bgIng" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+      <div class="paragraph-video">
+      <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet diam sit amet quam mattis maximus. 
+            Proin congue justo vitae diam facilisis mattis. Vestibulum nec suscipit massa. Sed odio tortor, faucibus eu mauris at, 
+            mollis tempus orci. Proin risus orci, imperdiet ac mauris et, egestas auctor leo. Integer erat nisl, eleifend ac laoreet
+            id, finibus eu arcu. In vitae diam vestibulum, gravida quam semper, accumsan justo. Suspendisse vehicula mauris id 
+            blandit finibus. Nunc dolor enim, cursus ut congue nec, euismod a turpis. Integer suscipit sapien sit amet ullamcorper 
+            lobortis. Mauris vitae bibendum nisl, et venenatis nulla. Duis sollicitudin rutrum ipsum nec luctus. Maecenas hendrerit 
+            lacinia ultricies. Fusce vel quam ac dui condimentum volutpat vel vel elit. Aenean eget felis vitae nunc porta malesuada. 
+            Duis efficitur orci nec suscipit viverra. In dictum, arcu vel blandit pretium, dolor metus hendrerit ligula, quis maximus 
+            erat ex ac lorem. Nunc ornare quam sed condimentum pretium. In dapibus lectus quis dolor vehicula elementum. 
+            Duis sollicitudin nisl et faucibus pellentesque. Cras dictum erat at massa eleifend euismod.
+            </p>
+      </div>
+  </div>
+    </section>
+<?php    
+getFooterOpen(); 
 ?>
+  <script src="js/collapse.js"></script>
+</html>
