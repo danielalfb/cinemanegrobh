72d47f0121a4ceb7c3678f1bc2afe103237f3e27
Lidiane Pereira <lidiane.pereira@dtidigital.com.br>
2021-04-06 17:48:44 -0300

    ADICIONANDO TRADUCAO PARA INGLES

--- semanadecinemanegro/function.php
+++ semanadecinemanegro/function.php
@@ -9,7 +9,6 @@ function getHeader() {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Semana de Cinema Negro de Belo Horizonte</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="./js/main.js"></script>
     <link rel="stylesheet" type="text/css" href="./css/main.css" />
     <link rel="stylesheet" type="text/css" href="./css/screen.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
@@ -65,63 +64,18 @@ function getHeader() {
            <button type="submit" id="searchButton"><em class="fa fa-search"></em></button>
            </form>
            </div>
-          </ul>
+         </ul>
         </div>
-        </div>
+      </div>
+      <div class="language">
+        <ul>
+          <li id="pt_click" class="button_lang current_lang">PT</li>
+          <li id="en_click" class="button_lang">EN</li>
+        </ul>
+      </div>
       </div>
   </div>
  <?php }
-
-function getFooter() { ?>
-  <!-- FOOTER -->
-  <div class="patrocinio"><img src="./img/regua-patrocinioCentralizada.svg" alt=""></div>
-  <footer class="footer-distributed">
-
-<div class="footer-left">
-  <a href="./inicio.php"><img src="./img/logo_branca.svg" class="logo" alt="Logo Footer"/></a>
-  <div>
-          <ul>
-            <li><a href="./sessao_de_abertura.php">Maria José Novais Oliveira - Nossa atriz</a></li>
-            <li><a href="./mostra_fespaco.php">Cinemas Africanos em revista: as origens do FESPACO</a></li>
-            <li><a href="./mostra_surreal16.php">Surreal16 Collective, Um Novo Olhar para o Cinema Nigeriano</a></li>
-            <li><a href="./mostra_homenagem.php">Cinema, negritude e poesia: uma homenagem a Sarah Maldoror</a></li>
-            <li><a href="./cine_escrituras_pretas.php">Cine-Escrituras Pretas</a></li>
-        </ul>
-    </div>
-</div>
-
-<div class="footer-center">
-      <ul>
-            <li><a href="./oficinas.php">Oficinas</a></li>
-            <li><a href="./debates.php">Debates</a></li>
-            <li><a href="./programacao.php">Programação</a></li>
-            <li><a href="./catalogo.php">Catálogo</a></li>
-            <li><a href="./creditos.php">Créditos</a></li>
-          </ul>
-</div>
-<div class="footer-right">
-  <p class="footer-about">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit
-      amet diam sit amet quam mattis maximus. Proin congue justo vitae
-      diam facilisis mattis. Vestibulum nec suscipit massa. Sed odio
-      tortor, faucibus eu mauris at, mollis tempus orci. Proin risus orci,
-      imperdiet ac mauris et, egestas auctor leo. Integer erat nisl,
-      eleifend ac laoreet id, finibus eu arcu. In vitae diam vestibulum,
-      gravida quam semper, accumsan justo. Suspendisse vehicula mauris id
-      blandit finibus.
-
-</p>
-</div>
-<div class="border"></div>
-<div class="footer-icons">
-    <a href="#"><em class="fab fa-instagram"></em></a>
-    <a href="#"><em class="fab fa-facebook-f"></em></a>
-    <a href="#"><em class="fab fa-youtube"></em></a>
-  </div>
-</footer>
-    </body>
-  </html>
-<?php }
 
 function getFooterOpen() { ?>
 <div class="patrocinio"><img src="./img/regua-patrocinioCentralizada.svg" alt=""></div>
@@ -169,4 +123,5 @@ function getFooterOpen() { ?>
     <a href="#"><em class="fab fa-youtube"></em></a>
   </div>
 </footer>
+<script src="js/main.js"></script>
 <?php }
--- semanadecinemanegro/inicio.php
+++ semanadecinemanegro/inicio.php
@@ -7,30 +7,70 @@ getHeader();
     <section>
       <div class="container-inicio center">
         <img class="inicio" src="img/inicio.png" alt="Foto em família ilustrada"/>
-        <div class="paragrafo-inicio">
-          <h3>Sobre a semana</h3>
-          <p>
-          A Semana de Cinema Negro de Belo Horizonte apresenta, 
-          de 10 a 16 de abril de 2021, um conjunto composto por 50 filmes 
-          de cinematografias negras brasileiras, africanas e da diáspora 
-          distribuídos em 3 mostras e 2 homenagens:  "Cinemas Africanos 
-          em revista: as origens do FESPACO"; "Surreal16 Collective, Um 
-          Novo Olhar para o Cinema Nigeriano"; "Cine-Escrituras Pretas"; 
-          "Sessão Maria José Novais Oliveira: Nossa atriz"; e "Cinema, 
-          negritude e poesia: uma homenagem a Sarah Maldoror”.
-          </p>
-          <p>
-          As sessões fílmicas estarão disponibilizadas, de forma on-line e gratuita, na plataforma TodesPlay (https://todesplay.com.br/), gerida pela APAN - Associação do Audiovisual Negro. Nesta edição, além das mostras, teremos conversas com convidadas e convidados internacionais, bem como debates com realizadoras e realizadores na mostra dedicada aos filmes brasileiros, todos serão exibidos no canal do Youtube da Semana de Cinema Negro. Contamos, ainda, com duas oficinas: "Programar filmes e construir imaginários: a potência do ofício da curadoria no cinema" ministrada por Heitor Augusto e “QuilomboCinema” ministrada por Tatiana Carvalho Costa.
-          </p>
-          <p>A primeira edição da Semana de Cinema Negro de Belo Horizonte propõe um olhar de resgate às nossas memórias. Estamos vivendo um momento de crise sanitária e econômica mundial, por conta da pandemia de Covid-19, e se faz necessário visitar lembranças para despertar o desejo de vivê-las novamente. Para isso, também, elaboramos um catálogo com textos que refletem sobre as nossas mostras, como a dedicada ao FESPACO (Festival Panafricano de Cinema e Televisão de Ouagadougou que acontece em Burkina Faso) que completou mais de 50 anos de existência, teremos um texto sobre o cinema nigeriano, com atenção especial às produções do coletivo Surreal16, sobre o cinema de Sarah Maldoror, sobre a presença de Maria José Novais Oliveira, nossa querida Dona Zezé, no cinema e sobre o processo de curadorias negras que perspectivam, a partir das próprias experiências, com olhar renovado para esse espaço, o da curadoria.</p>
-          <p>Convidamos a todas, todes e todos a acompanhar a programação.</p>       
-          <div class="btn-inicio">
-            <a href="/" class="btn" title="Link da plataforma">Link da plataforma</a>
-            <a href="/programacao.php" class="btn" title="Programação">Programação</a>
-            <a href="/catalogo.php" class="btn" title="Catálogo">Catálogo</a>
+            <div class="paragrafo-inicio">
+                <h3 id="pt">Sobre a semana</h3>
+                <h3 id="en">About the Week</h3>
+              <p id="pt">
+              A Semana de Cinema Negro de Belo Horizonte apresenta, 
+              de 10 a 16 de abril de 2021, um conjunto composto por 50 filmes 
+              de cinematografias negras brasileiras, africanas e da diáspora 
+              distribuídos em 3 mostras e 2 homenagens:  "Cinemas Africanos 
+              em revista: as origens do FESPACO"; "Surreal16 Collective, Um 
+              Novo Olhar para o Cinema Nigeriano"; "Cine-Escrituras Pretas"; 
+              "Sessão Maria José Novais Oliveira: Nossa atriz"; e "Cinema, 
+              negritude e poesia: uma homenagem a Sarah Maldoror”.
+              </p>
+              <p id="en">
+              From April 10th to 16th, 2021, the Belo Horizonte Black Film Week will present a selection of 50 films 
+              from Brazilian, African, and Diasporic black cinematography organized into 3 sections and 2 tributes: 
+              “African Films Revisited: the origins of FESPACO”; “Surreal16 Collective, A New Look at Nigerian Cinema”; 
+              “Black Film-Writings”; “Maria José Novais Oliveira: Our Actress”; and “Film, negritude and poetry: a tribute to Sarah Maldoror”.
+              </p>
+              <p id="pt">
+                As sessões fílmicas estarão disponibilizadas, de forma on-line e gratuita, na plataforma TodesPlay (https://todesplay.com.br/), 
+                gerida pela APAN - Associação do Audiovisual Negro. Nesta edição, além das mostras, teremos conversas com convidadas e convidados 
+                internacionais, bem como debates com realizadoras e realizadores na mostra dedicada aos filmes brasileiros, todos serão exibidos 
+                no canal do Youtube da Semana de Cinema Negro. Contamos, ainda, com duas oficinas: "Programar filmes e construir imaginários: 
+                a potência do ofício da curadoria no cinema" ministrada por Heitor Augusto e “QuilomboCinema” ministrada por Tatiana Carvalho Costa.
+              </p>
+              <p id="en">
+               The film programs will be made available for viewing online and free of charge, on  TodesPlay (https://todesplay.com.br), 
+                a platform managed by APAN – Associação do Audiovisual Negro [Association of Black Audiovisual]. In this edition, besides 
+                the sections, we will have talks with the international guests, and Q&A's with filmmakers in the section dedicated to 
+                Brazilian films, all of which will be broadcast on the Semana de Cinema Negro de Belo Horizonte’s YouTube channel. We
+                 will also have two workshops: “The power in film programming and reshaping the collective imaginary” lectured by Heitor 
+                 Augusto and “QuilomboCinema” lectured by Tatiana Carvalho Costa.
+              </p>
+              <p id="pt">A primeira edição da Semana de Cinema Negro de Belo Horizonte propõe um olhar de resgate às nossas memórias. 
+                Estamos vivendo um momento de crise sanitária e econômica mundial, por conta da pandemia de Covid-19, e se faz necessário 
+                visitar lembranças para despertar o desejo de vivê-las novamente. Para isso, também, elaboramos um catálogo com textos que 
+                refletem sobre as nossas mostras, como a dedicada ao FESPACO (Festival Panafricano de Cinema e Televisão de Ouagadougou que 
+                acontece em Burkina Faso) que completou mais de 50 anos de existência, teremos um texto sobre o cinema nigeriano, com atenção 
+                especial às produções do coletivo Surreal16, sobre o cinema de Sarah Maldoror, sobre a presença de Maria José Novais Oliveira,
+                 nossa querida Dona Zezé, no cinema e sobre o processo de curadorias negras que perspectivam, a partir das próprias experiências,
+                  com olhar renovado para esse espaço, o da curadoria.
+                </p>
+                <p id="en">
+                The first edition of Belo Horizonte Black Film Week proposes an outlook of retrieval of 
+                our memories. Due to the Covid-19 pandemic, we are living in a moment of worldwide sanitary 
+                and economic crisis and it is necessary to visit memories that awaken the desire of reliving them.
+                Therefore, we also prepared a catalog with texts that reflect on our sections such as the one dedicated
+                to FESPACO (Panafrican Film and Television Festival of Ouagadougou) that celebrated more than 50 years 
+                of existence. We will also have a text about the Nigerian Cinema, paying special attention to the
+                Surreal16 Collective productions. In parallel, there will be one text about Sarah Maldoror’s cinema, 
+                one about the presence of Maria José novais (our dear Dona Zezé) on film, and one text discussing 
+                the black curatorial process that put into perspective, from their own experiences, with a renewed 
+                gaze to this field, the one of curatorship.
+                </p>
+              <p id="pt">Convidamos a todas, todes e todos a acompanhar a programação.</p>       
+              <p id="en">We invite all of you to keep up with the program.</p>
+              <div class="btn-inicio">
+                <a href="/" class="btn" title="Link da plataforma">Link da plataforma</a>
+                <a href="/programacao.php" class="btn" title="Programação">Programação</a>
+                <a href="/catalogo.php" class="btn" title="Catálogo">Catálogo</a>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
     </section>
     <section>
     <div class="video">
