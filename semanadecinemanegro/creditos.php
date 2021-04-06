961445c4485635831f8de988d10d4466f357d3cd
danielalfb <danielalfbarbosa@gmail.com>
2021-04-06 16:04:51 -0300

    conteúdo e alinhamento dos créditos

--- semanadecinemanegro/creditos.php
+++ semanadecinemanegro/creditos.php
@@ -32,10 +32,103 @@ getHeader();
     <section>
       <div class="container">
         <div class="teamContainer">
-          <div class="teamSing">
-            <h1>Layla Braz</h1>
-            <h2>Coordenação Geral/ Direção Artística</h2>
-          </div>
+          <ul class="teamSing">
+            <li><h1>Coordenação Geral/ Direção Artística</h1></li>
+            <li><h3>Layla Braz</h3></li></br>
+            <li><h1>Produção Executiva</h1></li>
+            <li><h3>Layla Braz</h3></li></br>
+            <li><h1>Produção</h1></li>
+            <li><h3>Jacson Dias</h3></li></br></br></br>
+            <li><h1>Curadorias</h1></li>
+            <li><h2>Sessão Maria José Novais Oliveira: Nossa atriz</h2></li>
+            <li><h3>Alessandra Brito</h3></li>
+            <li><h3>André Novais Oliveira</h3></li></br>
+            <li><h2>Cine-Escrituras Pretas</h2></li>
+            <li><h3>Natalie Matos / Renca Produções</h3></li>
+            <li><h3>Tatiana Carvalho Costa</h3></li>
+            <li><h3>Vanessa Santos</h3></li></br>
+            <li><h2>Cinemas Africanos em revista: As origens do FESPACO</h2></li>
+            <li><h2>Cinema, Negritude e Poesia: Uma homenagem a Sarah Maldoror</h2></li>
+            <li><h2>Surreal16 Collective, Um novo olhar para o Cinema Nigeriano</h2></li>
+            <li><h3>Janaína Oliveira</h3></li>
+          </ul>
+          <ul class="teamSing">
+            <li><h1>Produção de Cópias</h1></li>
+            <li><h3>Carolina Martins</h3></li></br>
+            <li><h1>Ministrantes de Oficina</h1></li>
+            <li><h2>Programar filmes e construir imaginários: a potência do ofício da curadoria no cinema</h2></li>
+            <li><h3>Heitor Augusto</h3></li></br>
+            <li><h2>QuilomboCinema</h2></li>
+            <li><h3>Tatiana Carvalho Costa</h3></li></br>
+            <li><h1>Arte, Identidade Visual, Projeto Gráfico e Diagramação</h1></li>
+            <li><h3>Marco Chagas</h3></li></br>
+            <li><h1>Coordenação e produção editorial</h1></li>
+            <li><h3>Glaura Cardoso Vale / Rosa de Areia </h3></li></br>
+            <li><h1>Assistente de produção Editorial</h1></li>
+            <li><h3>Breno Henrique </h3></li></br>
+            <li><h1>Organização de Traduções e Revisão</h1></li>
+            <li><h3>Matheus Pereira / Rosa de Areia </h3></li>
+          </ul>
+          <ul class="teamSing">
+            <li><h1>Tradução</h1></li>
+            <li><h3>Aretha Soyombo</h3></li>
+            <li><h3>Cátia Maringolo</h3></li>
+            <li><h3>Henrique Cotrim</h3></li>
+            <li><h3>Ícaro Melo</h3></li>
+            <li><h3>Karoline Melo</h3></li>
+            <li><h3>Nathalia Dias</h3></li></br>
+            <li><h1>Gestão e Assessoria Jurídica</h1></li>
+            <li><h3>Diana Gebrim</h3></li>
+            <li><h3>Sociedade Individual de Advogados Diversidade 
+Gestão e Desenvolvimento de Projetos</h3></li></br>
+            <li><h1>Assistente de Gestão</h1></li>
+            <li><h3>Andreza Vieira</h3></li></br>
+            <li><h1>Website</h1></li>
+            <li><h2>Design e Desenvolvimento Front-end</h2></li>
+            <li><h3>Daniela Barbosa</h3></li><br>
+            <li><h2>Desenvolvimento Full Stack</h2></li>
+            <li><h3>Lidiane Mara</h3></li></br>
+          </ul>
+          <ul class="teamSing">
+            <li><h1>TodesPlay</h1></li>
+            <li><h3>Uilton Oliveira</h3></li>
+            <li><h3>Thais Scabio</h3></li>
+            <li><h3>Ana Caroline Brito</h3></li>
+            <li><h3>Rafael Ferreira</h3></li></br>
+            <li><h1>Vinheta</h1></li>
+            <li><h3>Higor Gomes</h3></li>
+            <li><h3>Maick Hannder</h3></li></br>
+            <li><h1>Assessoria de Imprensa</h1></li>
+            <li><h3>Mariana Cordeiro</h3></li></br>
+            <li><h1>Desenvolvimento de Material de Divulgação</h1></li>
+            <li><h3>Patrícia Fernandes</h3></li></br>
+          </ul>
+          <ul class="teamSing">
+            <li><h1>Redes Sociais</h1></li>
+            <li><h3>Andreza Vieira </h3></li></br>
+            <li><h1>Autoração de Cópias</h1></li>
+            <li><h3>Luisa Lanna</h3></li>
+            <li><h3>Marcela Santos</h3></li></br>
+            <li><h1>Assistente de Autoração de Cópias</h1></li>
+            <li><h3>Higor Gomes</h3></li>
+            <li><h3>Maick Hannder</h3></li></br>
+            <li><h1>Tradução e Legendagem</h1></li>
+            <li><h3>Juan Rodrigues</h3></li>
+            <li><h3>Pedro Veras</h3></li>
+            <li><h3>Luís Felipe Flores</h3></li>
+            <li><h3>Matheus Pereira</h3></li></br>
+          </ul>
+          <ul class="teamSing">
+            <li><h1>Legenda Descritiva</h1></li>
+            <li><h3>Matheus Guifer</h3></li>
+            <li><h3>Gabriel Aquino</h3></li></br>
+            <li><h1>Libras</h1></li>
+            <li><h3>Bruna Michele Pereira</h3></li>
+            <li><h3>Flávia Ribeiro Coelho de Carvalho</h3></li></br>
+            <li><h1>Captação de Recursos</h1></li>
+            <li><h3>Formata Cultural LTDA/ Izadora Fernandes</h3></li>
+          </ul>
+
         </div>
       </div>
     </section>
