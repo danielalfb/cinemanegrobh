39ecc310c444fdb40da84d30db6dd6d24bda128b
danielalfb <danielalfbarbosa@gmail.com>
2021-04-06 18:14:35 -0300

    página de error

--- semanadecinemanegro/error_400.php
+++ semanadecinemanegro/error_400.php
@@ -7,11 +7,11 @@ getHeader();
       <div class="errorContainer">
         <div class="errorInfo">
           <h1 id="pt">Ops!</h1>
-          <!-- <h1 id="en">Ops!</h1> -->
-          <h1 id="pt">O link que você buscou está indisponível!</h1></br>
-          <!-- <h1 id="en">The link is unavailable!</h1></br> -->
-          <h2 id="pt">Este filme ainda não foi disponibilizado.</h2>
-          <!-- <h2 id="en">The movie is not yet available.</h2> -->
+          <h1 id="en">Ops!</h1>
+          <h1 id="pt">O link está indisponível!</h1>
+          <h1 id="en">The link is unavailable!</h1>
+          <h2 id="pt">O filme ainda não foi disponibilizado.</h2>
+          <h2 id="en">The movie is not yet available.</h2>
         </div>
         <div class="errorIllus"><img src="img/error-future.png" alt="" /></div>
       </div>
