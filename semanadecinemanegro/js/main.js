e84b002b7b027f332cfafccd78048d23df7d9742
danielalfb <danielalfbarbosa@gmail.com>
2021-04-07 09:14:59 -0300

    tradução dos botões funcionando

--- semanadecinemanegro/js/main.js
+++ semanadecinemanegro/js/main.js
@@ -7,48 +7,55 @@
 })(jQuery);
 
 function translate() {
-    var portuguese = document.getElementById('pt_click'),
+  var portuguese = document.getElementById('pt_click'),
     english = document.getElementById('en_click'),
     pt_txt = document.querySelectorAll('#pt'),
     en_txt = document.querySelectorAll('#en'),
     nb_pt = pt_txt.length,
     nb_en = en_txt.length;
 
-portuguese.addEventListener('click', function() {
-    langue(portuguese,english);
-}, false);
+  portuguese.addEventListener(
+    'click',
+    function () {
+      langue(portuguese, english);
+    },
+    false
+  );
 
-english.addEventListener('click', function() {
-    langue(english,portuguese);
-}, false);
+  english.addEventListener(
+    'click',
+    function () {
+      langue(english, portuguese);
+    },
+    false
+  );
 
-function langue(langueOn,langueOff){
+  function langue(langueOn, langueOff) {
     if (!langueOn.classList.contains('current_lang')) {
-        langueOn.classList.toggle('current_lang');
-        langueOff.classList.toggle('current_lang');
+      langueOn.classList.toggle('current_lang');
+      langueOff.classList.toggle('current_lang');
     }
-    if(langueOn.innerHTML == 'PT'){
-        afficher(pt_txt, nb_pt);
-        cacher(en_txt, nb_en);
+    if (langueOn.innerHTML == 'PT') {
+      afficher(pt_txt, nb_pt);
+      cacher(en_txt, nb_en);
+    } else if (langueOn.innerHTML == 'EN') {
+      afficher(en_txt, nb_en);
+      cacher(pt_txt, nb_pt);
     }
-    else if(langueOn.innerHTML == 'EN'){
-        afficher(en_txt, nb_en);
-        cacher(pt_txt, nb_pt);
+  }
+  function afficher(txt, nb) {
+    for (var i = 0; i < nb; i++) {
+      txt[i].style.display = 'flex';
     }
-}
-function afficher(txt,nb){
-    for(var i=0; i < nb; i++){
-        txt[i].style.display = 'block';
+  }
+  function cacher(txt, nb) {
+    for (var i = 0; i < nb; i++) {
+      txt[i].style.display = 'none';
     }
-}
-function cacher(txt,nb){
-    for(var i=0; i < nb; i++){
-        txt[i].style.display = 'none';
-    }
-}
-function init(){
-    langue(portuguese,english);
-}
-init();
+  }
+  function init() {
+    langue(portuguese, english);
+  }
+  init();
 }
 translate();
